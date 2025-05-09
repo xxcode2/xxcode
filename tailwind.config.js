a17e1b38cb2ba6511b4f64c84320fa55b84d8b6b
xxcode2 <137141202+xxcode2@users.noreply.github.com>
2025-05-09 19:24:36 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,14 @@ module.exports = {
         }
       },
       animation: {
+        'fadeIn': 'fadeIn 0.5s ease-in-out',
         'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      keyframes: {
+        fadeIn: {
+          'from': { opacity: '0', transform: 'translateY(20px)' },
+          'to': { opacity: '1', transform: 'translateY(0)' }
+        }
       }
     },
   },
